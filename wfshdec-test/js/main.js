4bd64de94d949565144b3d7355753b4db7dc3838
cloud-driver <177719531+cloud-driver@users.noreply.github.com>
2025-06-28 18:44:06 +0800

    update

--- wfshdec-test/js/main.js
+++ wfshdec-test/js/main.js
@@ -289,3 +289,12 @@ document.addEventListener('click', function(e) {
   }
 });
 
+window.addEventListener('scroll', function () {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 200) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
